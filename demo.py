c39c5896ada5c61b7e4a90143bef543c453a6e05
Mobarakol Islam <mobarakol@u.nus.edu>
2021-03-10 07:34:13 +0800

    updated demo

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import os
 import numpy as np
 import random
@@ -9,38 +10,45 @@ import torch.optim as optim
 import torch.nn.functional as F
 from dataset_brats import dataset_brats19
 from model import UNet3D
-from utils import dice_3D, surface_dice_3D, seed_everything, worker_init_fn
+from utils import dice_3D, seed_everything, worker_init_fn, EDiceLoss
+from calibration_metrics import ece_eval, tace_eval, reliability_diagram
+import warnings
+warnings.filterwarnings("ignore")
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-def validate(valid_loader, model, args):
-    w, h = 0, args.num_classes
-    dice_valid = [[0 for x in range(w)] for y in range(h)]
-    wt_dice = []
-    surface_dice = [[0 for x in range(w)] for y in range(h)]
-    wt_surface_dice = []
+
+def step_valid(data_loader, model, metric):
+    ece_all, acc_all, conf_all, Bm_all, tace_all = [], [], [], [], []
+    losses, metrics, metrics_sd = [], [], []
     model.eval()
-    with torch.no_grad():
-        for batch_idx, (inputs, labels,_) in enumerate(valid_loader):
-            inputs = Variable(inputs).to(device)
-            outputs_orig = model(inputs)
-            outputs = outputs_orig.data.max(1)[1].squeeze_(1).cpu().numpy()
-            labels = np.array(labels.squeeze(1))
-            for bt in range(labels.shape[0]):
-                labs = np.unique(labels[bt])
-                dsc = dice_3D(np.logical_or(outputs[bt] == 1, outputs[bt] == 3).astype(np.uint8), np.logical_or(labels[bt] == 1, labels[bt] == 3).astype(np.uint8), 1)
-                dice_valid[1].append(dsc)
-                sd = surface_dice_3D(np.logical_or(outputs[bt] == 1, outputs[bt] == 3).astype(np.uint8), np.logical_or(labels[bt] == 1, labels[bt] == 3).astype(np.uint8), 1, args)
-                surface_dice[1].append(sd)
-                for cls_idx in range(2, len(labs)):
-                    dsc = dice_3D(outputs[bt], labels[bt], labs[cls_idx])
-                    dice_valid[cls_idx].append(dsc)
-                    sd = surface_dice_3D(outputs[bt], labels[bt], labs[cls_idx], args)
-                    surface_dice[cls_idx].append(sd)
-                dsc_w = dice_3D((outputs[bt] > 0).astype(np.uint8), (labels[bt] > 0).astype(np.uint8), 1)
-                sd_w = surface_dice_3D((outputs[bt] > 0).astype(np.uint8), (labels[bt] > 0).astype(np.uint8), 1, args)
-                wt_dice.append(dsc_w)
-                wt_surface_dice.append(sd_w)
-    return dice_valid, wt_dice, surface_dice, wt_surface_dice
+    for i, batch in enumerate(data_loader):
+        inputs, targets,_ = batch
+        targets = targets.squeeze().cuda(non_blocking=True)
+        inputs = inputs.float().cuda()
+        segs = model(inputs)
+        outputs = F.softmax(segs, dim=1).detach().cpu().numpy()
+        if len(targets.shape) < 4:#if batch size=1
+            targets = targets.unsqueeze(0)
+        labels = targets.detach().cpu().numpy()
+        
+        ece, acc, conf, Bm = ece_eval(outputs,labels)
+        tace, _, _, _ = tace_eval(outputs,labels)
+        ece_all.append(ece)
+        acc_all.append(acc)
+        conf_all.append(conf)
+        Bm_all.append(Bm)
+        tace_all.append(tace)
+        segs = segs.data.max(1)[1].squeeze_(1)
+        metric_ = metric.metric_brats(segs, targets)
+        metrics_sd.extend(metric.get_surface_dice(segs.detach().cpu().numpy(), targets.detach().cpu().numpy()))
+        metrics.extend(metric_)
+
+    ece_avg = np.stack(ece_all).mean(0)
+    acc_avg = np.stack(acc_all).mean(0)
+    conf_avg = np.stack(conf_all).mean(0)
+    Bm_avg = np.stack(Bm_all).mean(0)
+    tace_avg = np.stack(tace_all).mean(0)
+    return metrics, metrics_sd, ece_avg, acc_avg, conf_avg, Bm_avg, tace_avg
 
 def main():
     parser = argparse.ArgumentParser(description='Brats Training')
@@ -56,36 +64,41 @@ def main():
     parser.add_argument('--sd_tolerance', default=[1,2], type=int, help="Surface dice tolerance")
     parser.add_argument('--spacing_mm', default=(1,1,1), type=int, help="Surface dice spacing")
     parser.add_argument('--ckpt_dir', default='ckpt_brats19/', help='data root')
-    args = parser.parse_args(args=[])
+    args = parser.parse_args()
 
     dataset_valid = dataset_brats19(args=args, isTrain=False)
     valid_loader = DataLoader(dataset=dataset_valid, batch_size=args.batch_size, shuffle=False, num_workers=2,
                                worker_init_fn=worker_init_fn)
     
     print('Length of dataset- valid:', dataset_valid.__len__())
-    model = UNet3D(in_channels=args.in_channels, out_channels=args.num_classes, isSoftmax=True).to(device)
+    model = UNet3D(in_channels=args.in_channels, out_channels=args.num_classes, isSoftmax=False).to(device)
     model = torch.nn.parallel.DataParallel(model)
-    mode_list = ['best_oh.pth.tar', 'best_ls0.1.pth.tar', 'best_ls0.2.pth.tar', 'best_ls0.3.pth.tar', 'best_svls.pth.tar']
-    for mode in mode_list:
-        model.load_state_dict(torch.load(args.ckpt_dir + str(mode)))
-        dice_valid, wt_dice, surface_dice, wt_sd = validate(valid_loader, model, args)
-        avg_wt_sd = np.array(wt_sd).mean(axis=0)
-        avg_wt_std = np.array(wt_sd).std(axis=0)
-        avg_dice = []
-        std_dice = []
-        avg_sd = []
-        std_sd = []
-        for idx_eval in range(1, len(dice_valid)):
-            avg_dice.append(np.mean(dice_valid[idx_eval]))
-            std_dice.append(np.std(dice_valid[idx_eval]))
-            avg_sd.append(np.array(surface_dice[idx_eval]).mean(axis=0))
-            std_sd.append(np.array(surface_dice[idx_eval]).std(axis=0))
+    criterion_dice = EDiceLoss().to(device)
+    metric = criterion_dice.metric_brats
+    legends = ['OH', 'LS(0.1)', 'LS(0.2)', 'LS(0.3)', 'SVLS']
+    model_list = ['best_oh.pth.tar', 'best_ls0.1.pth.tar', 'best_ls0.2.pth.tar', 'best_ls0.3.pth.tar', 'best_svls.pth.tar']
+    for model_name, legend in zip(model_list, legends):
+        model.load_state_dict(torch.load(args.ckpt_dir + str(model_name)))
+        model.eval()
+        with torch.no_grad():
+            dice_metrics, metrics_sd, ece_avg, acc_avg, conf_avg, Bm_avg, tace_avg = step_valid(valid_loader, model, criterion_dice)
+        if legend != 'LS(0.3)':
+            reliability_diagram(conf_avg, acc_avg, legend=legend)
+        dice_metrics = list(zip(*dice_metrics))
+        dice_metrics = [torch.tensor(dice, device="cpu").numpy() for dice in dice_metrics]
+        avg_dices = np.mean(dice_metrics,1)
+        avg_std = np.std(dice_metrics,1)
+
+        metrics_sd = list(zip(*metrics_sd))
+        metrics_sd = [torch.tensor(dice, device="cpu").numpy() for dice in metrics_sd]
+        avg_sd = np.mean(metrics_sd,1)
+        avg_std_sd = np.std(metrics_sd,1)
+
+        print('model:%s , dice[ET:%.3f ± %.3f, TC:%.3f ± %.3f, WT:%.3f ± %.3f], ECE:%.4f, TACE:%.4f'%(
+            model_name, avg_dices[0],avg_std[0], avg_dices[1],avg_std[1], avg_dices[2],avg_std[2], ece_avg, tace_avg))
         
-        print('Smoothing:%s' % mode,'Cases:%d'%len(wt_dice), ' WT: %.3f ± %.3f '%(np.mean(wt_dice),np.std(wt_dice)), ' ET: %.3f ± %.3f '%(avg_dice[2],std_dice[2]),
-              ' TC: %.3f ± %.3f '%(avg_dice[0],std_dice[0]))
-        for tol_idx in range(len(args.sd_tolerance)):
-            print('tolerance: %d  WT: %.3f ± %.3f ET: %.3f ± %.3f TC: %.3f ± %.3f'%(args.sd_tolerance[tol_idx], avg_wt_sd[tol_idx], avg_wt_std[tol_idx], avg_sd[2][tol_idx],
-            std_sd[2][tol_idx], avg_sd[0][tol_idx], std_sd[0][tol_idx]))
+        print('model:%s , Surface dice[ET:%.3f ± %.3f, TC:%.3f ± %.3f, WT:%.3f ± %.3f]'%(
+            model_name, avg_sd[0],avg_std_sd[0], avg_sd[1],avg_std_sd[1], avg_sd[2],avg_std_sd[2]))
 
 if __name__ == '__main__':
     seed_everything()
